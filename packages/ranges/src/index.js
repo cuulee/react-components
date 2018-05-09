786bceae0c31d4c359da036dc409eeba68bc76b7
Austin Green <agreen@zendesk.com>
2018-05-09 14:39:01 -0700

    Rebase onto master and update references

--- packages/ranges/src/index.js
+++ packages/ranges/src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 export { default as Range } from './elements/Range';
 export { default as RangeField } from './elements/RangeField';
 export { default as Hint } from './views/Hint';
--- packages/ranges/src/views/Hint.js
+++ packages/ranges/src/views/Hint.js
@@ -1,8 +1,15 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import classNames from 'classnames';
-import { retrieveTheme, isRtl } from '@zendesk/garden-react-theming';
-import RangeStyles from '@zendesk/garden-css-forms/dist/range.css';
+import { retrieveTheme, isRtl } from '@zendeskgarden/react-theming';
+import RangeStyles from '@zendeskgarden/css-forms/dist/range.css';
 
 import { version } from '../../package.json';
 const COMPONENT_ID = 'ranges.hint';
